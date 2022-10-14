7e80eae8d34c15ffc4130f8913577842dc206aad
Rui Soares <rui.rprs3@gmail.com>
2022-10-14 19:05:12 +0100

    separacao e documentacao

---
+++ growTree.hs
@@ -0,0 +1,77 @@
+import MinceString
+
+data Arv a = Vazia 
+            | NoSoma Char (Arv a) (Arv a) -- soma entre dois termos
+            | NoProd Char (Arv a) (Arv a) -- produto entre cada elemento de um termo
+            | NoPoten Char (Arv a) (Arv a) --  potencia de uma variavel
+            | NoVar String                 -- variavel em string
+            | NoNum Int deriving Show      -- coeficiente
+
+-- Construir a arvore : Feito
+-- 1. Separar o polinomio pela metade   :  [[["5"],["x","2"]],[["-10"],["y","3"],["x","2"]]] -> [["5"],["x","2"]]  &  [["-10"],["y","3"],["x","2"]]
+        -- 1.5 separa-los na arvore com '+'
+
+-- 2. Separar os termos pela metade   :   [["5"],["x","2"]] ->  ["5"]   &   ["x","2"]
+        -- 2.5 separa-los com '*'
+
+-- 3. Separar as variaveis das potencias : ["x","2"] ->  "x"   &   "2"
+        -- 3.5 separa-los com  '^'
+
+
+---- Chama todas as funcoes e transforma um input do tipo [[["7"],["x","2"]]] -> NoProd '*' (NoNum 7) (NoPoten '^' (NoVar "x") (NoNum 2))
+---- tambem separa os termos de uma expressao com NoSoma's
+separaPoli :: [[[String]]] -> Arv a
+separaPoli lCoef
+        | head lCoef == [] = separaPoli (tail lCoef)
+        | s == 0 = separaCoef (head lCoef)
+        | otherwise = NoSoma '+' (separaPoli (take s lCoef)) (separaPoli (drop s lCoef))
+        where s = (length lCoef) `div` 2
+
+
+-- separa as variaveis dentro de um termo com NoProd's
+separaCoef :: [[String]] -> Arv a
+separaCoef coef
+        | s == 0 = separaFolha (head coef)
+        | otherwise = NoProd '*' (separaCoef (take s coef)) (separaCoef (drop s coef))
+        where s = (length coef) `div` 2
+
+
+-- decide se uma folha da arvore e um numero ou uma variavel
+separaFolha :: [String] -> Arv a
+separaFolha e
+        | s == 1 = numOuVar (head e)
+        | otherwise = NoPoten '^' (NoVar (head e)) (NoNum (read (head (tail e)) :: Int))
+        where s = length e
+
+
+-- verifica se uma string e um numero ou nao
+numOuVar :: String -> Arv a
+numOuVar e
+        | v = NoNum (read e :: Int)
+        | otherwise = NoPoten '^'  (NoVar e) (NoNum 1)
+        where v = checkIfDigit e
+
+
+
+-- verifica se uma string e um digito ou nao
+checkIfDigit :: String -> Bool
+checkIfDigit [] = True
+checkIfDigit (x:xs)
+        | v = checkIfDigit xs
+        | otherwise = False
+        where v = myIsDigit x
+
+-- funcao auxiliar para vetrificar se um char e um digito
+myIsDigit :: Char -> Bool
+myIsDigit '0' = True
+myIsDigit '1' = True
+myIsDigit '2' = True
+myIsDigit '3' = True
+myIsDigit '4' = True
+myIsDigit '5' = True
+myIsDigit '6' = True
+myIsDigit '7' = True
+myIsDigit '8' = True
+myIsDigit '9' = True
+myIsDigit '-' = True
+myIsDigit _ = False
---
+++ minceString.hs
@@ -0,0 +1,52 @@
+module MinceString where
+
+
+-- Parse da string : Feito
+-- 1. Receber a string 
+-- 2. Partir os "+"
+-- 3. Partir os "*" - problema quando tem 1, nao tem variavel x
+-- 4. Partir os "^" - problema quando tem 0 pq nao tem "^", quando tem 1 porque nao existe o "1"
+
+
+---- Chamada de todas as funções que transforma um input do tipo: "5*x^2" -> [[["5"],["x","2"]]]
+trataString :: String -> [[[String]]]
+trataString s = tiraVezes (tiraMais (simpMenos (removeEspaco s)))
+
+
+---- Remoção de todas as instancias de espaços para efeitos de simplificação da String de Input
+removeEspaco :: String -> String
+removeEspaco s = [x | x <- s, x/=' ']
+
+
+---- Adicionar o char '+' antes de todos os chars '-' porque a separação dos termos é feita a partir dos '+'
+simpMenos :: String -> String
+simpMenos [] = ""
+simpMenos (x:xs)
+        | x == '^' = x : head xs : simpMenos (tail xs)
+        | x == '-' = "+-" ++ simpMenos xs
+        | otherwise =  x : simpMenos xs
+
+
+---- Separar os termos pelo char '+' que transforma um input do tipo: "x+5*y+7*z^2" -> ["x", "5*y", "7*z^2"]
+tiraMais :: String -> [String]
+tiraMais s =  split '+' s
+
+
+---- Separa os componentes de um termo que transforma um input do tipo: ["x", "5*y", "7*z^2"] -> [[["x"]],[["5"],["y"]],[["7"],["z^2"]]]
+tiraVezes :: [String] -> [[[String]]]
+tiraVezes [] = []
+tiraVezes (x:xs) =  [tiraPotencia (split '*' x)] ++ tiraVezes xs
+
+
+---- Separa todos os elementos de uma string numa lista de strings, em que o caracter de separacao pode ser escolhido
+split :: Char -> String -> [String]
+split _ "" = []
+split c s = firstWord : (split c rest)
+    where firstWord = takeWhile (/=c) s
+          rest = drop (length firstWord + 1) s
+
+
+---- Separa os compontentes de uma variavel elevada a uma dada potencia. O que corresponde a uma operacao do tipo ["z^2"] -> [["z","2"]]
+tiraPotencia :: [String] -> [[String]]
+tiraPotencia  [] = []
+tiraPotencia (x:xs) = [split '^' x] ++ tiraPotencia xs
